2297ff0732799fada981a302de316816e44267d8
mdrokz <mohammadmunshi@gmail.com>
2022-12-14 14:14:30 +0530

    add base widget for displaying dispatched orders

---
+++ lib/dispatched.dart
@@ -0,0 +1,17 @@
+// generate a stateful widget dispatched
+
+import 'package:flutter/material.dart';
+
+class Dispatched extends StatefulWidget {
+  const Dispatched({Key? key}) : super(key: key);
+
+  @override
+  _DispatchedState createState() => _DispatchedState();
+}
+
+class _DispatchedState extends State<Dispatched> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
