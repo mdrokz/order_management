76c1f9bd12aef2b7bb354a130a5dec9068cd89f7
mdrokz <mohammadmunshi@gmail.com>
2022-12-15 19:01:40 +0530

    implement Fauna Model & Repo for orders

---
+++ lib/models/order.dart
@@ -0,0 +1,56 @@
+import 'package:faunadb_data/faunadb_data.dart';
+
+class Order extends Entity<Order> {
+  String id;
+  String companyName;
+  String orderDetails;
+  String orderStatus;
+  String createdAt;
+
+  Order(this.id, this.companyName, this.orderDetails, this.orderStatus,
+      this.createdAt);
+
+  @override
+  fromJson(Map<String, dynamic> model) {
+    return Order(
+      model['id'],
+      model['companyName'],
+      model['orderDetails'],
+      model['orderStatus'],
+      model['createdAt'],
+    );
+  }
+
+  @override
+  String getId() {
+    return id;
+  }
+
+  @override
+  Map<String, dynamic> model() {
+    return {
+      'id': id,
+      'companyName': companyName,
+      'orderDetails': orderDetails,
+      'orderStatus': orderStatus,
+      'createdAt': createdAt
+    };
+  }
+
+  static String collections() => 'Orders';
+  static String allOrders() => 'all_orders';
+}
+
+Order getOrderFromJson(Map<String, dynamic> json) {
+  return Order(
+    json['id'] as String,
+    json['companyName'] as String,
+    json['orderDetails'] as String,
+    json['orderStatus'] as String,
+    json['createdAt'] as String,
+  );
+}
+
+class OrderRepository extends FaunaRepository<Order> {
+  OrderRepository() : super(Order.collections(), Order.allOrders());
+}
